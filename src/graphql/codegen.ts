34b63fc793939a117ab34a328beff3ceccc628f9
andrew dremin <sankoa230@gmail.com>
2026-05-05 17:19:57 +0300

    feat: graphql init

---
+++ src/graphql/codegen.ts
@@ -0,0 +1,13 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema:
+    process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:3001/api/graphql",
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: ["typescript", "typescript-operations"],
+    },
+  },
+};
+
+export default config;
