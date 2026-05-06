f4187808e6ce7cc14a2594fcda63e6c9094fc4be
SimonSchulz <zenkov241091@gmail.com>
2026-05-06 17:15:49 +0300

    add empty pages for dashboard layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import { DashboardLayout } from '@/layouts/dashboard/ui/DashboardLayout'
+
+type Props = {
+    children: React.ReactNode
+}
+export default function Layout({children}: Props) {
+    return (
+        <DashboardLayout>
+            {children}
+        </DashboardLayout>
+    )
+}
--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,10 +1,5 @@
-import {DashboardLayout} from "@/layouts/dashboard/ui/DashboardLayout";
 import {UsersPage} from "@/app/(dashboard)/users/UsersPage";
 
 export default function UsersRoutePage() {
-    return (
-        <DashboardLayout>
-            <UsersPage />
-        </DashboardLayout>
-    )
+    return <UsersPage />
 }
